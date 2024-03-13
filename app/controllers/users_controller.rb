9926c3ac4577bfe989c5626c99e4032384db8acd
Mikhail8425 <mikhailshemet@gmail.com>
2024-03-13 13:09:49 -0400

    fixed redirecting bug after registration

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,7 +12,7 @@ class UsersController < ApplicationController
     user = User.new(user_params)
     if (user.save)
       session[:user_id] = user.id
-      redirect_to [:users, :index], notice: 'user created'
+      redirect_to '/products', notice: 'user created'
     else
       render :new
     end
